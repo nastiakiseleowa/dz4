530dbbd3d6a2ed9ff5ef1dfe1e2815c579fc4d55
Nastia Kiseleowa <nastia.kiseleowa@yandex.ru>
2022-08-22 23:49:23 +0300

    add task27

---
+++ task27/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число ");
+int Number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (Number > 0)
+{
+    sum = sum + Number % 10;
+    Number = Number / 10;
+}
+
+Console.WriteLine(sum);
---
+++ task29/Program.cs
@@ -0,0 +1,21 @@
+﻿string number = Console.ReadLine();
+Console.Write(number.Length);
+string[] array = new string[number.Length];
+int index = 0;
+for (int i = 0; i<=number.Length; i++)
+{
+    if (number[i] != ',')
+    {
+    array[index] += number[i].ToString();
+}
+    else
+{
+    index++;
+}
+}
+Console.Write("[ ");
+for (int i = 0; i <= index - 1; i++)
+{
+    Console.Write(array[i] + ", ");
+}
+Console.Write(array[index] + "]");
