6b3dc86dfc0c77f8f1784cf67b160ea355f12336
Nastia Kiseleowa <nastia.kiseleowa@yandex.ru>
2022-08-22 23:48:13 +0300

    add task25

---
+++ task25/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int number = A;
+
+for (int i = 1; i < B; i++)
+{
+    number = number * A;
+}
+Console.WriteLine(number);
